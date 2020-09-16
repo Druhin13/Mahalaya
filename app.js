031c9fc7ec6196835d833f0d16ecaa7b522190ea
Druhin Tarafder <druhinpro@gmail.com>
2020-09-17 01:17:59 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,2 +1,33 @@
-var audio = new Audio('assets/mahalaya.mp3');
-audio.play();
+//function play() {
+//	var audio = new Audio('https://www.mboxdrive.com/mahalaya.mp3');
+//	audio.play();
+//	document.getElementById("play").style.display = "none";
+//	document.getElementById("pause").style.display = "block";
+//}
+//
+//function pause() {
+//	var audio = new Audio('https://www.mboxdrive.com/mahalaya.mp3');
+//	audio.pause();
+//	document.getElementById("play").style.display = "block";
+//	document.getElementById("pause").style.display = "none";
+//}
+
+
+
+var myAudio = document.getElementById("myAudio");
+var isPlaying = false;
+
+function togglePlay() {
+	isPlaying ? myAudio.pause() : myAudio.play();
+	document.getElementById("play").style.display = "none";
+	document.getElementById("pause").style.display = "block";
+};
+
+myAudio.onplaying = function () {
+	isPlaying = true;
+};
+myAudio.onpause = function () {
+	isPlaying = false;
+	document.getElementById("play").style.display = "block";
+	document.getElementById("pause").style.display = "none";
+};
